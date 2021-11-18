4a972fcb05a4ddede055fc03e6e87d352991f642
Monttzo <dpulido59@gmail.com>
2021-11-18 12:22:55 -0500

    Se crea proyecto
    Se crea proyecto con vue.js, se dan las primeras configuraciones y se da inicio al desarrollo del inicio

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,98 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app" class="app">
+    <nav class="nav">
+      <div class="LogoBox" v-on:click="buttonHome" >
+        <img src="../public/aid-kit1.png" class="logo">
+        <h1>DocBlogs</h1>
+      </div>
+      <div class="ButtonLogInBox">
+        <button v-on:click="buttonLogIn">Login</button>
+      </div>
+      <div class="NewPostBox">
+        <button v-on:click="buttonNewPost">New Post</button>
+      </div>
+      <h1>This is an example menu</h1>
+      <h1>Option 1</h1>
+      <h1>Option 2</h1> 
+    </nav>
   </div>
-  <router-view />
 </template>
+<script>
+export default {
+  name: 'App',
+  data: function(){
+    return {
 
+    }
+  },
+  methods:{
+
+  },
+  created: function(){
+
+  }
+}
+</script>
 <style>
+button {
+  padding: 15px 10px;
+  font-weight: bold;
+  font-size: 16px;
+  color:#8bd5cb;
+  background-color: #516091;
+  border-radius: 5px;
+  border-width: 0px;
+}
+.NewPostBox {
+  display: flex;
+  position: absolute;
+  right: 100px;
+  width: fit-content;
+}
+.ButtonLogInBox {
+  display: flex;
+  position: absolute;
+  right: 20px;
+  width: fit-content;
+}
+.LogoBox {
+  cursor: pointer;
+  display: flex;
+  width: fit-content;
+  float: left;
+  align-items: center;
+}
+.LogoBox h1 {
+  padding: 0px 10px;
+}
+.logo {
+  height: 45px;
+  width: 50px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #516091;
+  background-color: #edfffa;
+  margin: 0px;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
 }
-
-#nav {
-  padding: 30px;
+body {
+  margin: 0px;
 }
-
-#nav a {
+.nav {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  background-color: #8bd5cb;
   font-weight: bold;
-  color: #2c3e50;
+  color: #516091;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.nav h1 {
+  display: inline-block;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,14 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '../App.vue'
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
-
+const routes = [{
+  path: '/',
+  name: 'root',
+  component: App
+}]
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
+  history: createWebHistory(),
+  routes
+})
 
-export default router;
+export default router
